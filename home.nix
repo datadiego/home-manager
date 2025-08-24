455981c4e6fac037f77384dfeb685ff458e8b4c2
datadiego <juandiegomariscal@gmail.com>
2025-08-24 20:46:37 +0200

    añadimos feh y wallpaper

--- home.nix
+++ home.nix
@@ -7,19 +7,23 @@
   home.stateVersion = "24.11"; # Please read the comment before changing.
 
   home.packages = [
+	pkgs.vscodium
 	pkgs.rofi
 	pkgs.alacritty
 	pkgs.fira-code
 	pkgs.gh
 	pkgs.micro
 	pkgs.xclip
+
+	pkgs.feh
   ];
 
   home.file = {
 	".config/i3/config".source = dotfiles/i3;
 	".config/alacritty/alacritty.toml".source = dotfiles/alacritty.toml;
-        ".config/alacritty/themes".source = dotfiles/alacritty-themes;
+    ".config/alacritty/themes".source = dotfiles/alacritty-themes;
 	".bashrc".source = dotfiles/bashrc;
+	".config/micro/colorschemes/dracula.micro".source = dotfiles/dracula.micro;
   };
 
   home.sessionVariables = {
