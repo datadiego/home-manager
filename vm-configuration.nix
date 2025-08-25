3655b0ebf8963474dccdca57e09f544cb331d7a2
datadiego <juandiegomariscal@gmail.com>
2025-08-25 10:18:54 +0200

    configuracion ip estatica vm

--- vm-configuration.nix
+++ vm-configuration.nix
@@ -25,12 +25,12 @@ networking.networkmanager.enable = false;
 networking.interfaces.lan0 = {
   useDHCP = false;
   ipv4.addresses = [ {
-    address = "192.168.0.17";
+    address = "192.168.100.17";
     prefixLength = 24;
   } ];
 };
 
-networking.defaultGateway = "192.168.0.1";
+networking.defaultGateway = "192.168.100.1";
 networking.nameservers = ["1.1.1.1" "8.8.8.8"];
 
 services.udev.extraRules = ''
